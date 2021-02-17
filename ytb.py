0f59a176f8130ce011eb328a3b451555381b34c9
Legenda24 <48826466+Legenda24@users.noreply.github.com>
2021-02-18 03:24:26 +0500

    Update ytb.py
    + New regex (again)
    + Added additional info (pytube)
    + Now thumbnail gets through pytube too

--- ytb.py
+++ ytb.py
@@ -1,31 +1,49 @@
+from datetime import timedelta
+
 from pyrogram import Client, filters
-from pyrogram.errors import WebpageCurlFailed
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pytube import YouTube as yt
+from pytube.exceptions import VideoUnavailable
 
 
-app = Client('ytb') 
+app = Client("ytb")
 
 
-@app.on_message(filters.command('start'))
+@app.on_message(filters.command("start"))
 async def start_message(_, msg):
-    start_message = '''🇺🇸Send me valid link to YouTube video you want to get thumbnail of. 
+    start_message = """🇺🇸Send me valid link to YouTube video you want to get thumbnail of. 
 
 
-🇷🇺Отправь мне валидную ссылку на YouTube видео превью которого ты хочешь получить.'''
+🇷🇺Отправь мне валидную ссылку на YouTube видео превью которого ты хочешь получить."""
     await msg.reply(start_message)
 
 
-@app.on_message(filters.regex("^https:\/\/?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/).{11}$"))
+@app.on_message(filters.regex("^https?:\/\/?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/).{11}"))
 async def gyt(_, msg):
-    video_id = msg.text[-11:]
-    kb = InlineKeyboardMarkup([[InlineKeyboardButton('🔗Link', url=f"https://youtu.be/{video_id}")]])
-
     try:
-        await msg.reply_photo(f'http://img.youtube.com/vi/{video_id}/maxresdefault.jpg', quote=True, reply_markup=kb)
-    except WebpageCurlFailed:
+        vid_url = msg.matches[0].group(0)
+        yt_vid = yt(vid_url)
+
+        vid_turl = yt_vid.thumbnail_url
+        vid_title = yt_vid.title
+        vid_author = yt_vid.author
+        vid_length = timedelta(seconds=yt_vid.length)
+        vid_pdate = yt_vid.publish_date.strftime("%Y-%m-%d")
+
+        kb = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔗Link", url=f"https://youtu.be/{vid_url[-11:]}")]])
+        await msg.reply_photo(
+            vid_turl,
+            caption=f"""🎬 **{vid_title}**
+👤 `{vid_author}`
+
+⏳ **{vid_length}**
+🗓 `{vid_pdate}`""",
+            quote=True,
+            reply_markup=kb,
+        )
+    except VideoUnavailable:
         await msg.reply("**Invalid video link!**", quote=True)
-    except:
-        await msg.reply_photo(f'http://img.youtube.com/vi/{video_id}/0.jpg', quote=True, reply_markup=kb)
 
 
 app.run()
